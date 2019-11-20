ac93dbdb90f7f1fe811f32b0fa2b36903a2ff8c6
manuel-bettinelli <57376425+manuel-bettinelli@users.noreply.github.com>
2019-11-20 19:59:30 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+package trabalho.eng.pkg2;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+      String numero1=JOptionPane.showInputDialog("Digite o primeiro número:");
+      String numero2=JOptionPane.showInputDialog("Digite o segundo número");
+      String operador=JOptionPane.showInputDialog("Digite o operador");
+      String MostreResultado = null;
+
+      {
+          switch (operador) {
+              case "+":
+                  MostreResultado = String.valueOf(Float.parseFloat(numero1) + Float.parseFloat(numero2));
+                  break;
+              case "-":
+                  MostreResultado =String.valueOf(Float.parseFloat(numero1) - Float.parseFloat(numero2));
+                  break;
+              case "/":
+                  MostreResultado =String.valueOf(Float.parseFloat(numero1) / Float.parseFloat(numero2));
+                  break;
+              case "*":
+                  MostreResultado =String.valueOf(Float.parseFloat(numero1) * Float.parseFloat(numero2));
+                  break;
+              default:
+                  break;
+          }
+  JOptionPane.showMessageDialog(null,"O Resultado é:"+ MostreResultado);
+      System.exit( 0 );
+          
+      }
+    }
+    
+}
