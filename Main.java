5715a00bd88ecb864d1831d95fb0f61dece3ea7f
manuel-bettinelli <57376425+manuel-bettinelli@users.noreply.github.com>
2019-11-27 21:12:37 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
       String numero1=JOptionPane.showInputDialog("Digite o primeiro número:");
       String numero2=JOptionPane.showInputDialog("Digite o segundo número");
-      String operador=JOptionPane.showInputDialog("Digite o operador");
       String MostreResultado = null;
 
       {
